d543b38ae7f88ed5f0ef8d9e0ddc62b5750b00a1
d67217777-cloud <d67217777@gmail.com>
2026-07-25 22:12:03 +0530

    Delete hooks directory

--- hooks/SceneManagerContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react';
-import type { SceneManager } from './useSceneManager';
-
-export const SceneManagerContext = createContext<SceneManager | null>(null);
-
-export function useSceneManagerContext(): SceneManager | null {
-  return useContext(SceneManagerContext);
-}
